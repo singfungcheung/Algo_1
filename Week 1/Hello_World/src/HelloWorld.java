dae37db4d3f40f2fa1044718a76c5f3dbb927410
Sing Fung Cheung <singfung.cheung@gmail.com>
2022-05-13 19:19:25 -0400

    Added some files

--- Week 1/Hello_World/src/HelloWorld.java
+++ Week 1/Hello_World/src/HelloWorld.java
@@ -2,5 +2,9 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello, World");
+//        for (int i = 0; i < args.length; i++)
+//        {
+//            System.out.println(args[i]);
+//        }
     }
 }
