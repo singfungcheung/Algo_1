767ef56429cf1d9c346e65deb54f80c5762ce3cf
Sing Fung Cheung <singfung.cheung@gmail.com>
2022-05-13 21:33:07 -0400

    first draft completion

--- Week 1/Hello_World/src/RandomWord.java
+++ Week 1/Hello_World/src/RandomWord.java
@@ -1,5 +1,30 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 public class RandomWord {
     public static void main(String[] args) {
-        
+        // Read the input
+//        String name = StdIn.readString();
+//        if (StdIn.isEmpty()) {
+//            System.out.print("Empty");
+//        } else {
+//            System.out.println("Not Empty");
+//        }
+//        System.out.println(name);
+        String name = "";
+        String winner = "";
+        int counter = 0;
+        while (!StdIn.isEmpty()) {
+            counter++;
+            name = StdIn.readString();
+//            System.out.println(name);
+            float prob = (float) 1/counter;
+//            System.out.println(prob);
+            if (StdRandom.bernoulli(prob)) {
+                winner = name;
+            }
+        }
+        System.out.println(winner);
+
     }
 }
